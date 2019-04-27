e5f101cc880d484d97a35c70b1b957ca7becf12b
Vladimir <shikarno_3000@inbox.ru>
2019-04-27 05:43:58 +0300

    form validation done

--- build/js/index.js
+++ build/js/index.js
@@ -1,99 +1,60 @@
 "use strict";
 
 {
-  var applicationForm = document.forms.application;
+  var applicationForm = document.forms.application; // Создаем ошибку
+
+  var createError = function createError(text) {
+    var error = document.createElement("div");
+    error.className = "text-error";
+    error.textContent = text;
+    return error;
+  }; // Сбрасываем текст ошибки, чтобы он не дублировался
+
+
+  var clearErrors = function clearErrors() {
+    var errors = applicationForm.querySelectorAll('.text-error');
+    errors.forEach(function (error) {
+      error.remove();
+    });
+  }; // Валидируем все поля по регуляркам и для каждого поля с проваленной валидацией, очищаем его
+
+
+  var checkInputs = function checkInputs(input, pattern, text) {
+    if (!input.value.match(pattern)) {
+      // <--Валидируем
+      var error = createError(text);
+      input.parentNode.appendChild(error);
+      input.value = ''; // <--Очищаем
+    }
+  };
 
   var formValidate = function formValidate(evt) {
     var name = applicationForm.elements.name,
         email = applicationForm.elements.email,
         tel = applicationForm.elements.tel,
         textarea = applicationForm.elements.textarea,
+        inputs = applicationForm.querySelectorAll('.feedback-form__input'),
         namePattern = /[а-яё]{2,}\s+[а-яё]{2,}/i,
         emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
         telPattern = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/,
         textareaPattern = /[а-яё\d]+(\s+[а-яё\d]+)?/;
 
-    if (!name.value.match(namePattern)) {
+    if (!name.value.match(namePattern) || !email.value.toLowerCase().match(emailPattern) || !tel.value.match(telPattern) || !textarea.value.match(textareaPattern)) {
       evt.preventDefault();
-      var error = document.createElement("div");
-      error.textContent = 'Укажите, пожалуйста, фамилию и имя';
-      error.className = "text-error";
-      name.parentNode.appendChild(error);
-      toggleNameError();
+      inputs.forEach(function (input) {
+        clearErrors();
+
+        if (!input.value) {
+          checkInputs(name, namePattern, 'Укажите, пожалуйста, фамилию и имя');
+          checkInputs(email, emailPattern, 'Укажите, пожалуйста, фамилию и имя');
+          checkInputs(tel, telPattern, 'Укажите, пожалуйста, номер телефона');
+          checkInputs(textarea, textareaPattern, 'Пожалуйста, напишите о себе');
+        }
+      });
+    } else {
+      applicationForm.submit();
     }
-
-    if (!email.value.toLowerCase().match(emailPattern)) {
-      evt.preventDefault();
-
-      var _error = document.createElement("div");
-
-      _error.textContent = 'Укажите, пожалуйста, email';
-      _error.className = "text-error";
-      email.parentNode.appendChild(_error);
-    }
-
-    if (!tel.value.match(telPattern)) {
-      evt.preventDefault();
-
-      var _error2 = document.createElement("div");
-
-      _error2.textContent = 'Укажите, пожалуйста, номер телефона';
-      _error2.className = "text-error";
-      tel.parentNode.appendChild(_error2);
-    }
-
-    if (!textarea.value.match(textareaPattern)) {
-      evt.preventDefault();
-
-      var _error3 = document.createElement("div");
-
-      _error3.textContent = 'Пожалуйста, напишите о себе';
-      _error3.className = "text-error";
-      textarea.parentNode.appendChild(_error3);
-    }
-  }; // const showError = (label, textError) => {
-  //   label.classList.add('error');
-  //   const inputError = document.createElement("div");
-  //   inputError.classList.add('textError');
-  //   inputError.textContent = textError;
-  // }
-  // const hideError = (label) => {
-  //   label.classList.remove('error');
-  //   if (label.lastElementChild.classList.contains('error-message')) {
-  //     label.lastChild.remove();
-  //   };
-  // }
-  // const formValidate = (evt) => {
-  //   const name = applicationForm.elements.name,
-  //         email = applicationForm.elements.email,
-  //         tel = applicationForm.elements.tel,
-  //         textarea = applicationForm.elements.textarea,
-  //         namePattern = /[а-яё]{2,}\s+[а-яё]{2,}/i,
-  //         emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-  //         telPattern = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/,
-  //         textareaPattern = /[а-яё\d]+(\s+[а-яё\d]+)?/;
-  //   hideError(name.parentNode);
-  //   if (!name.value.match(namePattern)) {
-  //     showError(name.parentNode, 'Укажите, пожалуйста, фамилию и имя');
-  //     evt.preventDefault();
-  //   }
-  //   hideError(email.parentNode);
-  //   if (!email.value.toLowerCase().match(emailPattern)) {
-  //     showError(email.parentNode, 'Укажите, пожалуйста, email');
-  //     evt.preventDefault();
-  //   }
-  //   hideError(tel.parentNode);
-  //   if (!tel.value.match(telPattern)) {
-  //     showError(tel.parentNode, 'Укажите, пожалуйста, номер телефона');
-  //     evt.preventDefault();
-  //   }
-  //   hideError(textarea.parentNode);
-  //   if (!textarea.value.match(textareaPattern)) {
-  //     showError(textarea.parentNode, 'Пожалуйста, напишите о себе');
-  //     evt.preventDefault();
-  //   }
-  // }
-
+  };
 
   applicationForm.addEventListener("submit", formValidate);
 }
--- src/js/validate.js
+++ src/js/validate.js
@@ -1,105 +1,59 @@
 {
   const applicationForm = document.forms.application;
+  
+  // Создаем ошибку
+  const createError = text => {
+    const error = document.createElement("div");
+    error.className = "text-error";
+    error.textContent = text;
+    return error;
+  }
+
+  // Сбрасываем текст ошибки, чтобы он не дублировался
+  const clearErrors = () => {
+    const errors = applicationForm.querySelectorAll('.text-error');
+    errors.forEach(error => {
+      error.remove();
+    })
+  }
+
+  // Валидируем все поля по регуляркам и для каждого поля с проваленной валидацией, очищаем его
+  const checkInputs = (input, pattern, text) => {
+    if (!input.value.match(pattern)) { // <--Валидируем
+      const error = createError(text)
+      input.parentNode.appendChild(error);
+      input.value = ''; // <--Очищаем
+    }
+  }
 
   const formValidate = evt => {
     const name = applicationForm.elements.name,
           email = applicationForm.elements.email,
           tel = applicationForm.elements.tel,
           textarea = applicationForm.elements.textarea,
+          inputs = applicationForm.querySelectorAll('.feedback-form__input'),
           namePattern = /[а-яё]{2,}\s+[а-яё]{2,}/i,
           emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
           telPattern = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/,
           textareaPattern = /[а-яё\d]+(\s+[а-яё\d]+)?/;
-    
-    if (!name.value.match(namePattern)) {
-      evt.preventDefault();
-      const error = document.createElement("div");
-      error.textContent = 'Укажите, пожалуйста, фамилию и имя';
-      error.className = "text-error";
-      name.parentNode.appendChild(error);
-      toggleNameError();
-    }
 
-    if (!email.value.toLowerCase().match(emailPattern)) {
+    if (!name.value.match(namePattern) || !email.value.toLowerCase().match(emailPattern) || !tel.value.match(telPattern) || !textarea.value.match(textareaPattern)) {
       evt.preventDefault();
-      const error = document.createElement("div");
-      error.textContent = 'Укажите, пожалуйста, email';
-      error.className = "text-error";
-      email.parentNode.appendChild(error);
-    }
 
-    if (!tel.value.match(telPattern)) {
-      evt.preventDefault();
-      const error = document.createElement("div");
-      error.textContent = 'Укажите, пожалуйста, номер телефона';
-      error.className = "text-error";
-      tel.parentNode.appendChild(error);
-    }
+      inputs.forEach(input => {
+        clearErrors();
 
-    if (!textarea.value.match(textareaPattern)) {
-      evt.preventDefault();
-      const error = document.createElement("div");
-      error.textContent = 'Пожалуйста, напишите о себе';
-      error.className = "text-error";
-      textarea.parentNode.appendChild(error);
-    }
+        if (!input.value) {
+            checkInputs(name, namePattern, 'Укажите, пожалуйста, фамилию и имя')
+            checkInputs(email, emailPattern, 'Укажите, пожалуйста, фамилию и имя')
+            checkInputs(tel, telPattern, 'Укажите, пожалуйста, номер телефона')
+            checkInputs(textarea, textareaPattern, 'Пожалуйста, напишите о себе')
+          }
+      })
 
-    
+    } else {
+      applicationForm.submit()
+    }
   }
-
-  // const showError = (label, textError) => {
-  //   label.classList.add('error');
-  //   const inputError = document.createElement("div");
-  //   inputError.classList.add('textError');
-  //   inputError.textContent = textError;
-  // }
-
-  // const hideError = (label) => {
-  //   label.classList.remove('error');
-  //   if (label.lastElementChild.classList.contains('error-message')) {
-  //     label.lastChild.remove();
-  //   };
-  // }
-
-  // const formValidate = (evt) => {
-    
-  //   const name = applicationForm.elements.name,
-  //         email = applicationForm.elements.email,
-  //         tel = applicationForm.elements.tel,
-  //         textarea = applicationForm.elements.textarea,
-  //         namePattern = /[а-яё]{2,}\s+[а-яё]{2,}/i,
-  //         emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-  //         telPattern = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/,
-  //         textareaPattern = /[а-яё\d]+(\s+[а-яё\d]+)?/;
-
-  //   hideError(name.parentNode);
-
-  //   if (!name.value.match(namePattern)) {
-  //     showError(name.parentNode, 'Укажите, пожалуйста, фамилию и имя');
-  //     evt.preventDefault();
-  //   }
-
-  //   hideError(email.parentNode);
-
-  //   if (!email.value.toLowerCase().match(emailPattern)) {
-  //     showError(email.parentNode, 'Укажите, пожалуйста, email');
-  //     evt.preventDefault();
-  //   }
-
-  //   hideError(tel.parentNode);
-
-  //   if (!tel.value.match(telPattern)) {
-  //     showError(tel.parentNode, 'Укажите, пожалуйста, номер телефона');
-  //     evt.preventDefault();
-  //   }
-
-  //   hideError(textarea.parentNode);
-
-  //   if (!textarea.value.match(textareaPattern)) {
-  //     showError(textarea.parentNode, 'Пожалуйста, напишите о себе');
-  //     evt.preventDefault();
-  //   }
-  // }
-
   applicationForm.addEventListener("submit", formValidate);
 }
